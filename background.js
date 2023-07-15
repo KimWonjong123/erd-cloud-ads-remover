6fb724aaa5f8ff418c963b9f7b86c3e2920b9144
KimWonjong123 <bonk159@gmail.com>
2023-07-15 12:31:10 +0900

    fix bug

--- background.js
+++ background.js
@@ -1,19 +1,13 @@
-let tabId, currentUrl, url;
-
 chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
-    tabId = details.tabId;
-    currentUrl = details.url;
-}, { url: [{ hostSuffix: "erdcloud.com" }] });
-
-chrome.webRequest.onCompleted.addListener(details => {
-    url = new URL(details.url);
-    if (url.pathname.includes("d/") && tabId) {
+    let tabId = details.tabId;
+    let url = new URL(details.url);
+    if (tabId && url.pathname.includes("d/")) {
         chrome.tabs.sendMessage(tabId, { action: "removeAd" }, response => {
             if (response && response.status) {
                 setTimeout(() => {
                     chrome.tabs.setZoom(1.05)
                         .then(chrome.tabs.setZoom(0));
-                }, 1000);
+                }, 500);
             }
         });
     }
